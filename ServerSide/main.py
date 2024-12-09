f6ba5905b1c0035554dc156af0c12502f829e1d7
raoul <raoulmerotto1@gmail.com>
2024-12-09 17:38:38 +0100

    - Aggiunta disclaimer come route del server - Eliminazione ambiente di sviluppo utilizzato per l'iniziale creazione del server

--- ServerSide/main.py
+++ ServerSide/main.py
@@ -5,9 +5,12 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+#output di default all'accesso alla route del server con disclaimer
+#Le persone che hanno partecipato a questo progetto non hanno responsabilità
+#nella raccolta dei dati di chi accede a questo servizio
 @app.route("/")
 def home():
-    return "Home"
+    return "This server is hosting a service and every access is saved, nor the host or the takes accuntabilty for the inoformations collected."
 
 if __name__ == "__main__":
       app.run(debug = true)
