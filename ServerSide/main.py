d7ce38b5d6d7fd03e768e9064b89d863da86a0df
raoul <raoulmerotto1@gmail.com>
2024-12-09 18:34:25 +0100

    Creazione metodo ottenere i dettagli del plugin (GET) e per crearli sul server (POST)

--- ServerSide/main.py
+++ ServerSide/main.py
@@ -1,4 +1,5 @@
 # Punto d'ingresso del servizio
+from asyncio.windows_events import NULL
 from doctest import debug
 
 from flask import Flask, request, jsonify
@@ -10,7 +11,17 @@ app = Flask(__name__)
 #nella raccolta dei dati di chi accede a questo servizio
 @app.route("/")
 def home():
-    return "This server is hosting a service and every access is saved, nor the host or the takes accuntabilty for the inoformations collected."
+    return "This server is hosting a service and every access is saved, nor the host or the team of development takes accuntabilty for the inoformations collected."
+
+#Interfaccia con la tabella del database tramite GET e POST
+#Se la richiesta è un POST la funzione aggiunge il parametro passato come plugin nella tabella
+#Se la richiesta è un GET la funzione cerca il plugin tramite id nella tabella
+@app.route("/plugin_details",methods=["POST","GET"])
+def plugTable():
+    if request.method == "POST":
+        return NULL
+    else:
+        return NULL
 
 if __name__ == "__main__":
       app.run(debug = true)
