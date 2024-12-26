cf6c753924cb207f87d0d08b5c75dcddca3a5439
NicolaDellaPietaCasagrande <158739233+NicolaDellaPietaCasagrande@users.noreply.github.com>
2024-12-26 09:55:05 +0000

    pluginLoader con lista aggiornata

--- ServerSide/core/plugin_loader.py
+++ ServerSide/core/plugin_loader.py
@@ -1,13 +1,15 @@
 import os #libreria per poter lavorare con le cartelle e file dell'applicazione
 import importlib #va a sostituire la funzione manuale di import dato che non siamo a conoscenza dei nomi dei file
+import sys
+from pathlib import Path
 
 def caricaPlugin(folder, req, nome_plugin):
     plugin = 0
+
     for file in os.listdir(folder):
         if nome_plugin == file[:-3] and file.endswith('.py'):
 
-            percorso_modulo = folder+"."+nome_plugin 
-            modulo = importlib.import_module(percorso_modulo) #contenuto del file (plug in) py
+            modulo = importlib.import_module(nome_plugin) #contenuto del file (plug in) py
             
             if hasattr(modulo, req):
                 plugin = modulo  # Aggiungi il contetunto del file plug in nel vettore plugins
@@ -15,18 +17,28 @@ def caricaPlugin(folder, req, nome_plugin):
                 plugin = 1
     return plugin
 
+def lista_plugin(folder):
+    var = []
+    for file in os.listdir(folder):
+        if file[:-3] and file.endswith('.py'):
+            var.append(file)
+    return var
 
 # Requisiti: la funzione 'esegui' deve essere presente
 if(__name__ == "__main__"):
     print("Quale file PY vuoi eseguire?")
     nome_plugin = input()
     req = "execute"
-    folder = "plugin"
+    folder = Path(__file__).resolve().parent.parent / "plugins"  # Una cartella sopra quella corrente
+    sys.path.append(str(folder))
 
     # Carica i plugin validi
+    for i in lista_plugin(folder):
+        print(i)
+
     plugin = caricaPlugin(folder, req, nome_plugin)
 
-    if plugin != 0:
+    if plugin != 0 and plugin != 1:
         plugin.execute()
     elif plugin == 1:
         print("Il plugin" +nome_plugin+ "non rispetta i requisiti: " +req)
