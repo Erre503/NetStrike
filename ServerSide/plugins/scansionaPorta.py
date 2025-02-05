b4f47953c41f8164eccc62bf052bf3aa245530b7
TommasoGallon <158739256+TommasoGallon@users.noreply.github.com>
2025-02-05 12:10:22 +0100

    Update scansionaPorta.py

--- ServerSide/plugins/scansionaPorta.py
+++ ServerSide/plugins/scansionaPorta.py
@@ -1,15 +1,35 @@
+import threading
 import socket
 
-def execute(host, port):
-    print(f"Scansione porta {port} per {host} ")
+def check_port(host, port):
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  
-    if sock.connect_ex((host, port)) == 0: # Restituisce 0 se la connessione è riuscita
-        print(f"Porta {port} aperta")
-    else:
-        print(f"Porta {port} chiusa")
+    sock.settimeout(1)  # Timeout di 1 secondo
 
-    sock.close()
+    if sock.connect_ex((host, port)) == 0: # Restituisce 0 se la connessione è riuscita
+        print(f"Porta {port}: Aperta")
     
-execute("google.com", 81)
+    sock.close()
+
+    
+
+def execute(host, inizio, fine):
+    print(f"Scansione porta dalla {inizio} alla {fine}")
+    threads = []
+    for port in range(inizio, fine+1):
+        # Creazione di un thread per ogni porta
+        thread = threading.Thread(target = check_port, args = (host, port))
+        thread.daemon = True  # Imposta il thread come daemon: chiude il thread quando l'utente chiude il programma
+        threads.append(thread) # Aggiunge il thread al vettore threads
+        thread.start() # Esegue il thread
+
+    # Aspetta che tutti i thread terminino
+    for thread in threads:
+        thread.join() # Termina il programma quando tutti i thread finiscono
+
+    
+    
+
+execute("google.com", 1, 1)
+    
+    
