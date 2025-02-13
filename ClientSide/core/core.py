7ce2c0dad0901dcae635f25b7e7f7b53a12a2ce3
noob33344348 <88556997+noob33344348@users.noreply.github.com>
2025-02-13 12:07:34 +0100

    Logging
    Sistema di logging client-side funzionante.
    Non vengono memorizzati i dati inviati e ricevuto a '/login' e '/register'.

--- ClientSide/core/core.py
+++ ClientSide/core/core.py
@@ -9,7 +9,10 @@ import time
 import threading
 import utils.security_functions as sf
 import os
+import logging
 
+#Lista di endpoint di cui non loggare gli specifici dati inviati e ricevuti
+PRIVATE_DATA_ENDPOINTS = ('/login', '/register')
 
 """
 Classe principale per il lato client del core.
@@ -31,19 +34,31 @@ Attributes:
 
 class ClientCore:
     """
-    Inizializza il ClientCore con il server URL e il gestore dell'interfaccia utente.
+    Inizializza il ClientCore con il server URL, il gestore dell'interfaccia utente,
+    esegue login con le credenziali, configura il logger e avvia il poll degli aggiornamenti.
 
     Args:
         server_url (str): URL del server a cui inoltrare le richieste.
         ui_handler (UIUpdater): Oggetto che gestisce l'aggiornamento dell'interfaccia grafica.
+        username (str): Nome utente per effettuare il login
+        password (str): Password per effettuare il login
     """
 
     def __init__(self, server_url, ui_handler, username, password):
         self.server_url = server_url
         self.ui_handler = ui_handler
         self.last_update = round(time.time())
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(asctime)s - %(levelname)s - %(message)s',
+            handlers=[
+                logging.FileHandler("app.log"),
+                logging.StreamHandler()
+            ]
+        )
+        logging.info("Application started.")
+        logging.info("ClientCore initialized with server URL: %s", server_url)
         self.login(username, password)
-
         self.start_polling()
 
     """
@@ -66,10 +81,9 @@ class ClientCore:
             self.ui_handler.aggiorna_risultato_test(data)
 
         elif update_type == UpdateType.AGGIORNA_LISTA:
-            print("AGGIORNA LA CAZZO DI LISTA")
+            print("AGGIORNA LA CAZZO DI LISTA") #Chage
         else:
-            raise ValueError(f"Tipo di aggiornamento non riconosciuto: {update_type}")
-        # eccetera
+            logging.error("Type of UIUpdate unknown: %s",update_type)
 
     """
     Invia una richiesta al server e gestisce la relativa risposta.
@@ -99,12 +113,12 @@ class ClientCore:
         try:
             url = f"{self.server_url}{endpoint}"
             headers = {"Authorization": f"Bearer {sf.get_token()}"}
-            print(url)
-            if(dati != None and sanitize):
-                print("Sanifying...")
-                dati = sf.sanitize_dict(dati)
+            show_data= not(endpoint in PRIVATE_DATA_ENDPOINTS)
 
-            print("URL:",url + "\t DATA:",dati) #DEBUG
+            if show_data:
+                logging.debug("Sending %s request to %s with data: %s",metodo, url,dati)
+            else:
+                logging.debug("Sending %s request to %s", metodo, url)
 
             if metodo == "GET":
                 response = requests.get(url, headers=headers, verify='./certificates/server.crt')
@@ -113,21 +127,25 @@ class ClientCore:
             elif metodo == "PATCH":
                 response = requests.patch(url, json=dati, headers=headers, verify='./certificates/server.crt')
             else:
-                raise ValueError("Metodo HTTP non supportato.")
+                logging.error("Unsupported method: %s", metodo)
+                raise ValueError("Unsupported HTTP method.")
+
+            if show_data:
+                logging.debug("Received response: %s", response.text)
+            else:
+                logging.debug("Received response")
 
             if response.status_code == 200:
                 ret = response.json()
-                if(isinstance(ret, list)):
+                if isinstance(ret, list):
                     ret = sf.sanitize_list(ret)
                 else:
                     ret = sf.sanitize_dict(ret)
             else:
-                response = sf.sanitize_dict(response)
-                print(f"Errore: {response.status_code}: {response.text}")
+                logging.error("Error %s: %s", response.status_code, response.text)
 
         except Exception as e:
-            e = sf.sanitize_input(e)
-            print(f"Errore durante la richiesta: {e}")
+            logging.error("Error during request: %s", sf.sanitize_input(e))
 
         return ret
 
@@ -148,11 +166,15 @@ class ClientCore:
         - Stampa un messaggio di errore in caso di autenticazione fallita.
     """
     def login(self, username, password):
+        logging.info("Attempting to log in user: %s", username)
         token = self.invia_richiesta('/login', 'POST', {'username': username, 'password': password})['access_token']
         if token:
             sf.save_token(token)
+            logging.info("User %s logged in successfully.", username)
             return token
-        print('Errore di autenticazione')
+        logging.warning("Authentication failed for user: %s", username)
+
+
 
 
     """
@@ -168,12 +190,13 @@ class ClientCore:
         - Stampa un messaggio di errore in caso di registrazione fallita.
     """
     def register(self, username, password):
+        logging.info("Attempting to register user: %s", username)
         success = self.invia_richiesta('/register', 'POST', {'username': username, 'password': password})
         if success:
-            print('Registrato con successo')
+            logging.info("Registration successful for user: %s", username)
             self.login(username, password)
         else:
-            print('Errore di registrazione')
+            logging.warning("Registration failed for user: %s", username)
 
     """
     Esegue il logout dell'utente, rimuovendo il token di autenticazione.
@@ -184,7 +207,7 @@ class ClientCore:
     """
     def logout(self):
         sf.clear_token()
-        print("Logout effettuato con successo.")
+        logging.info("Logout effettuato con successo.")
 
 
     """
@@ -280,25 +303,22 @@ class ClientCore:
                 self.invia_richiesta('/upload_plugin', 'POST', {'content': file.read(), 'name': os.path.basename(file_path)}, False)
             self.ottieni_lista_plugin()
         except Exception as e:
-            print(f"Errore durante il caricamento del plugin: {e}")
+            logging.error(f"Errore durante il caricamento del plugin: {e}")
 
     """
     Modifica un plugin presente nel server.
 
     Args:
         id_plugin (str): L'ID del plugin da modificare.
-        data (str): Il nuovo valore per il campo specificato (nome o descrizione).
-        is_name (bool, opzionale): Se True, modifica il nome del plugin;
-                                    altrimenti modifica la descrizione.
-                                    Default: False.
+        name (str, opzionale): Il nuovo valore per il campo specificato nome, impostato a default None.
+        description (str, opzionale): Il nuovo valore per il campo specificato nome, impostato a default None.
 
     Effetti:
         - Invia una richiesta PATCH all'endpoint '/edit_plugin/<id_plugin>' con
-          i dati aggiornati.
-        - Specifica il campo da modificare (nome o descrizione) in base al valore di `is_name`.
+          i dati modificati.
     """
-    def modifica_plugin(self, id_plugin, data, is_name=False):
-        self.invia_richiesta('/edit_plugin/'+id_plugin, 'PATCH', { 'name' if is_name else 'description': data })
+    def modifica_plugin(self, id_plugin, name=None, description=None):
+        self.invia_richiesta('/edit_plugin/'+id_plugin, 'PATCH', { 'name':name, 'description': description })
 
     """
     Avvia un thread separato per il polling delle notifiche dal server.
@@ -309,6 +329,7 @@ class ClientCore:
         - Il thread esegue continuamente il polling delle notifiche finché il programma è attivo.
     """
     def start_polling(self):
+        logging.info("Starting polling for notifications.")
         polling_thread = threading.Thread(target=self.poll_notifications)
         polling_thread.daemon = True
         polling_thread.start()
@@ -329,12 +350,14 @@ class ClientCore:
     """
     def poll_notifications(self):
         while True:
+            logging.debug("Polling for notifications...")
             dati = self.invia_richiesta("/notification/"+str(self.last_update))["update"]
-            if dati != None and dati > 0:
+            if dati is not None and dati > 0:
                 self.last_update = dati
+                logging.info("Received new notifications, updating UI.")
                 self.aggiorna_ui('', UpdateType.AGGIORNA_LISTA)
-
             time.sleep(5)
+
 
 
 """
